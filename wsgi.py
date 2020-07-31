162846e47f5dd6abfbe60e589f17f1d5ad98baae
Lorenz Vader <lorenz.pfaeffli@protonmail.com>
2020-08-01 00:17:27 +0200

    added additional route, added separate stfg method

--- wsgi.py
+++ wsgi.py
@@ -15,15 +15,31 @@ app = Flask(__name__)
 # setup cache
 cache = Cache(config={"CACHE_TYPE": "simple"})
 cache.init_app(app)
+# 5 minute cache timeout
+cache_timeout = 300
 
-
-# @cache.cached(timeout=540)
 @app.route('/feed')
-def stfg():
+@cache.cached(timeout=cache_timeout)
+def follow_feed():
     feed_type = request.args.get("ft", "rss")
-    channel = request.args.get("user", None)
+    return stfg(feed_type)
 
-    if channel is None:
+@app.route('/feed/<channel>')
+@cache.cached(timeout=cache_timeout)
+def channel_feed(channel):
+    feed_type = request.args.get("ft", "rss")
+    return stfg(feed_type, channel)
+
+@app.route("/favicon.ico")
+def favicon():
+    return send_from_directory(os.path.join(app.root_path, "media"), "favicon.ico", mimetype="image/vnd.microsoft.icon")
+
+def stfg(feed_type=None, channel=None, update=True):
+
+    if feed_type is None:
+        feed_type = "rss"
+
+    if update:
         mk.update()
 
     if feed_type == "rss":
@@ -34,10 +50,6 @@ def stfg():
         response = app.make_response(mk.atom_str(channel))
         response.headers.set("Content-Type", "application/atom+xml")
         return response
-
-@app.route("/favicon.ico")
-def favicon():
-    return send_from_directory(os.path.join(app.root_path, "media"), "favicon.ico", mimetype="image/vnd.microsoft.icon")
 
 if __name__ == "__main__":
       app.run(host="127.0.0.1", port=5000)
